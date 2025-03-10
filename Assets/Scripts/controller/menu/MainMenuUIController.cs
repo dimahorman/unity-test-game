71b3a99e3facbe87c4f6e83b1dd6d6af32425d83
Dima Horman <dimahorman@gmail.com>
2025-03-10 23:26:31 +0200

    Added UI window for PointAndClick with Load Game and New Game buttons, added transitioning between windows in main menu, tuned looks for settings pop up.

--- Assets/Scripts/controller/menu/MainMenuUIController.cs
+++ Assets/Scripts/controller/menu/MainMenuUIController.cs
@@ -1,26 +1,81 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuUIController : MonoBehaviour {
-    [SerializeField] SettingsPopUp settingsPopUp;
+    [SerializeField] private SettingsPopUp settingsPopUp;
+    [SerializeField] private PointAndClickWindow pointAndClickWindow;
+
+    [SerializeField] private GameObject startWindow;
+
+    private GameObject _currentWindow;
+
+    private readonly Stack<GameObject> _previousWindows = new();
 
     private void Start() {
         settingsPopUp.Close();
+        pointAndClickWindow.gameObject.SetActive(false);
+
+        _currentWindow = startWindow;
+
+        _currentWindow.SetActive(true);
     }
 
     public void OnSettings() {
-        
+        OpenNextWindow(settingsPopUp.gameObject);
     }
 
     public void OnPointAndClick() {
-        
+        OpenNextWindow(pointAndClickWindow.gameObject);
     }
 
     public void OnFirstPerson() {
-        
+        SceneManager.LoadScene($"Scenes/FirstPersonScene");
     }
 
     public void OnQuit() {
+        EditorApplication.isPlaying = false;
         Application.Quit();
+    }
+
+    public void GoToPreviousWindow() {
+        if (_previousWindows.Count > 0) {
+            StartCoroutine(FadeCurrentWindow(() => {
+                _currentWindow.SetActive(false);
+                _currentWindow = _previousWindows.Pop();
+
+                _currentWindow.SetActive(true);
+                var canvasGroup = _currentWindow.GetComponent<CanvasGroup>();
+                canvasGroup.interactable = true;
+                canvasGroup.alpha = 1;
+            }));
+        }
+    }
+
+    private void OpenNextWindow(GameObject window) {
+        StartCoroutine(FadeCurrentWindow(() => {
+            _currentWindow.SetActive(false);
+            _previousWindows.Push(_currentWindow);
+
+            _currentWindow = window;
+            window.SetActive(true);
+            var canvasGroup = window.GetComponent<CanvasGroup>();
+            canvasGroup.alpha = 1;
+            canvasGroup.interactable = true;
+        }));
+    }
+
+    private IEnumerator FadeCurrentWindow(Action afterFade) {
+        var canvasGroup = _currentWindow.GetComponent<CanvasGroup>();
+        canvasGroup.interactable = false;
+        while (canvasGroup.alpha > 0) {
+            canvasGroup.alpha -= Time.deltaTime * 6;
+            yield return null;
+        }
+
+        afterFade();
     }
 }
